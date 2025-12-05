8f02e211dc13806e7eda97cdf3078593692f0c83
Eric Vennemeyer <henri.barun@gmail.com>
2025-12-05 14:46:43 -0800

    2.7 -- Animating the Player

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 @export var move_speed: float = 100.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _ready() -> void:
 	pass
@@ -15,5 +17,17 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = move_vector * move_speed
+	
+	if velocity.x > 0:
+		animated_sprite_2d.play("move_right")
+	elif velocity.x < 0:
+		animated_sprite_2d.play("move_left")
+	elif velocity.y > 0:
+		animated_sprite_2d.play("move_down")
+	elif velocity.y < 0:
+		animated_sprite_2d.play("move_up")
+	
+	if velocity == Vector2.ZERO:
+		animated_sprite_2d.stop()
 	
 	move_and_slide()
