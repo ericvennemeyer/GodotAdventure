527ea4464ee7ae3f32e0cca58a7f7ebc4aac266c
Eric Vennemeyer <henri.barun@gmail.com>
2025-12-05 14:14:39 -0800

    2.5 -- Moving the Player

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
 
 
+@export var move_speed: float = 100.0
+
+
 func _ready() -> void:
 	pass
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("move_left"):
-		print("Move Left")
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = move_vector * move_speed
+	
+	move_and_slide()
