54a68252f4c9e46abcef1744b37507c253455bbe
Eric Vennemeyer <henri.barun@gmail.com>
2025-12-19 14:38:18 -0800

    5.4 -- Introducing User Interfaces

--- Scenes/NPC/npc.gd
+++ Scenes/NPC/npc.gd
@@ -1,6 +1,9 @@
 extends StaticBody2D
 
 
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
+
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact"):
-		print("Hello there!")
+		canvas_layer.visible = !canvas_layer.visible
