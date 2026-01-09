3e346aab85dd349c258584f48a07ead4f6555ffa
Eric Vennemeyer <henri.barun@gmail.com>
2026-01-09 14:32:23 -0800

    5.9 -- Multiple Lines of Dialogue

--- Scenes/NPC/npc.gd
+++ Scenes/NPC/npc.gd
@@ -2,16 +2,22 @@ extends StaticBody2D
 
 
 var can_interact: bool = false
-var dialogue_lines: Array[String] = ["One", "Two", "Three"]
+var dialogue_index: int = 0
+
+@export var dialogue_lines: Array[String] = ["One", "Two", "Three"]
 
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
-
-
-func _ready() -> void:
-	print(dialogue_lines[0], dialogue_lines[2])
+@onready var dialogue_label: Label = $CanvasLayer/DialogueLabel
 
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and can_interact:
-		canvas_layer.visible = !canvas_layer.visible
-		get_tree().paused = !get_tree().paused
+		if dialogue_index < dialogue_lines.size():
+			get_tree().paused = true
+			canvas_layer.visible = true
+			dialogue_label.text = dialogue_lines[dialogue_index]
+			dialogue_index += 1
+		else:
+			get_tree().paused = false
+			canvas_layer.visible = false
+			dialogue_index = 0
